d2dd90d69b15b1dc74ab80fa1a1dbd67a1f1c7c5
Lucas Parodi <lucas.parodi93@gmail.com>
2022-03-14 21:24:49 -0300

    API input

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
-fetch("https://pokeapi.co/api/v2/pokemon/155")
-    .then( (resp) => resp.json() )
-    .then( (data) => {
-        let dex = document.querySelector("#pokedex");
-        dex.innerHTML = `<p>${data.name}</p>
+let containerIn = document.querySelector(".containerIn");
+let buscar = document.querySelector(".buscar");
+
+containerIn = addEventListener("input", () => { //esto lo intenté de varias maneras, no sé si usar un search, un input o submit, ya probé los 3 y no me funcionó
+
+    fetch(`https://pokeapi.co/api/v2/pokemon/pikachu`) //aca la idea es que busque el nombre que entra por el input
+        .then((resp) => resp.json())
+        .then((data) => {
+            let dex = document.querySelector("#pokedex");
+            dex.innerHTML = `<p>${data.name}</p>
         <p>${(data.id)}</p>
         <img src='${data.sprites.front_default}'/>
         `
+            console.log(data)
+        })
 
 
 
-        console.log(data)
-    })
+})
+
