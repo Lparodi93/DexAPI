ee701a9aa962110b2286c9e83013dbb6f70cc8fd
Lucas Parodi <lucas.parodi93@gmail.com>
2022-03-16 00:20:23 -0300

    Api mod

--- main.js
+++ main.js
@@ -1,20 +1,23 @@
 let containerIn = document.querySelector(".containerIn");
 let buscar = document.querySelector(".buscar");
 
-containerIn = addEventListener("input", () => { //esto lo intenté de varias maneras, no sé si usar un search, un input o submit, ya probé los 3 y no me funcionó
+containerIn = addEventListener("submit", (e) => {
 
-    fetch(`https://pokeapi.co/api/v2/pokemon/pikachu`) //aca la idea es que busque el nombre que entra por el input
-        .then((resp) => resp.json())
-        .then((data) => {
-            let dex = document.querySelector("#pokedex");
-            dex.innerHTML = `<p>${data.name}</p>
-        <p>${(data.id)}</p>
-        <img src='${data.sprites.front_default}'/>
-        `
-            console.log(data)
-        })
-
-
-
+    e.preventDefault();
+    busqueda(buscar.value);
 })
 
+function busqueda(pokemon) {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`) //aca la idea es que busque el nombre que entra por el input
+        .then((resp) => resp.json())
+        .then((data) => {
+            pokeCreate(data);
+        })
+}
+
+function pokeCreate(miPoke) {
+    let dex = document.querySelector("#pokeData");
+    dex.innerHTML = `<p>${miPoke.name}</p>
+    <p>${(miPoke.id)}</p>
+    <img src='${miPoke.sprites.front_default}'/>`
+}
